9678f19e931c61602f5a71f28465b3f93f6a0120
DungNT <dungnt@navisoft.com.vn>
2023-09-04 14:48:52 +0700

    build(Modify Module constructor)

--- MLCpp/AntiSpoofPredict.h
+++ MLCpp/AntiSpoofPredict.h
@@ -1,10 +1,45 @@
-#pragma once
 #include "MiniFASNet.h";
 #include<torch/all.h>
-ref class AntiSpoofPredict
+#include<opencv2/opencv.hpp>
+#include<string>
+#include<vector>
+#pragma once
+namespace MLCpp
 {
-private:
-	
+    class Detection
+	{
+	private:
+		std::string* dirname;
+		cv::dnn::Net* detector;
+		float detector_confidence;
+	public:
+		Detection();
+		~Detection();
+		Detection(std::string model, std::string dir);
+		std::vector<int> getbbox(cv::Mat Img);
+	};
 
-};
+	
+	static std::map<std::string, std::function<MiniFASNet()>> Model_Dictionary = 
+	{ {"MiniFASNETV1", []() { return MiniFASNetV1(); } }, {"MiniFASNetV2" ,[]() {return MiniFASNetV2(); } },
+		{"MiniFASNetV1SE", []() {return MiniFASNetV1SE(); }},	{"MiniFASNetV2SE", []() {return MiniFASNetV2SE(); }} };
+
+
+	class AntiSpoofPredict
+	{
+	private:
+		std::string Device;
+		std::string ModelPath;
+
+		torch::nn::Module Model;
+	public:
+		AntiSpoofPredict();
+		AntiSpoofPredict(std::string model_name,std::string model_path, std::string device);
+		~AntiSpoofPredict();
+		void LoadModel(std::string ModelPath);
+		void predict(char img[]);
+
+	};
+}
+
 
