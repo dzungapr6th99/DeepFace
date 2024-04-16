91d0bb6ec039e12afce32d4a614c191e8b6cadf8
DESKTOP-U0VLU25\Admin <dzung.apr6th@gmail.com>
2024-04-16 11:33:54 +0700

    reconfig OpenCv Wrapper

--- DeepFace/Program.cs
+++ DeepFace/Program.cs
@@ -1,6 +1,8 @@
 using CommonLib;
 using DeepFace;
 using Microsoft.Extensions.Configuration;
+using System.Runtime.InteropServices;
+using Microsoft.OpenApi.Models;
 using NLog.Web;
 string ConfigFolder = "./ConfigApp"; //can be relative or fullpath
 string ConfigLogFile = "ConfigLog/nlog.config";
@@ -24,11 +26,37 @@ builder.WebHost.ConfigureAppConfiguration(
 });
 // Add services to the container.
 builder.Host.UseNLog();
-builder.Services.AddRazorPages();
 // Init configs
 ConfigData.InitConfigData(builder.Configuration);
 Starting.InitProject(builder.Services);
-
+builder.Services.AddEndpointsApiExplorer(); builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo
+    {
+        Title = "JWTToken_Auth_API",
+        Version = "v1"
+    });
+    c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme()
+    {
+        Name = "TOKEN",
+        Type = SecuritySchemeType.ApiKey,
+        Scheme = "Bearer",
+        BearerFormat = "JWT",
+        In = ParameterLocation.Header,
+        Description = "JWT Authorization header using the Bearer scheme. \r\n\r\n Enter 'Bearer' [space] and then your token in the text input below.\r\n\r\nExample: \"Bearer string_token\"",
+    });
+    c.AddSecurityRequirement(new OpenApiSecurityRequirement {
+        {
+            new OpenApiSecurityScheme {
+                Reference = new OpenApiReference {
+                    Type = ReferenceType.SecurityScheme,
+                        Id = "Bearer"
+                }
+            },
+            new string[] {}
+        }
+    });
+});
 // Add services to the container.
 
 var app = builder.Build();
@@ -41,13 +69,8 @@ if (!app.Environment.IsDevelopment())
     app.UseHsts();
 }
 
-app.UseHttpsRedirection();
-app.UseStaticFiles();
 
 app.UseRouting();
 
-app.UseAuthorization();
-
-app.MapRazorPages();
 
 app.Run();
--- PyFaceDetect/test.py
+++ PyFaceDetect/test.py
@@ -1,9 +1,16 @@
-from deepface import DeepFace
+from deepface import Deepface
 import cv2
+import datetime
 
 image_1 = cv2.imread("lee_1.jpg")
 image_2 = cv2.imread("lee_2.jpg")
-
-for i in range(3):
-    res = DeepFace.verify(image_1, image_2, model_name="VGG-Face", distance_metric="cosine", enforce_detection=False)
-    print(res)
+res = Deepface.verify(image_1, image_2, model_name="VGG-Face", distance_metric="cosine", enforce_detection=False)
+timeConsume = 0
+for i in range(10):
+    c1 = datetime.datetime.now
+    res = Deepface.verify(image_1, image_2, model_name="VGG-Face", distance_metric="cosine", enforce_detection=False)
+    c2 = datetime.datetime.now
+    c = (c2-c1)
+    timeConsume = timeConsume+c.milisecond
+    
+print( timeConsume /10)
