8047a4782250195c51373da893eee976d555c1e3
DungNT <dzung.apr6th@gmail.com>
2024-09-11 21:04:17 +0700

    feat(Add Qdrant Data Access)

---
+++ QdrantDA/Class1.cs
@@ -0,0 +1,17 @@
+﻿
+using Qdrant;
+using Qdrant.Client;
+using System.Security.Cryptography.X509Certificates;
+namespace QdrantDA
+{
+    public class Class1
+    {
+        public static void Main(string[] args)
+        {
+            QdrantClient _client = new QdrantClient(new Uri("localhost"))
+
+
+
+        }
+    }
+}
